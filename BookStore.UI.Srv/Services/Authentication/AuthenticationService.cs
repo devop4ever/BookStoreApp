4961ae3eafb80d8f007dcf6d743d0162c4c6242f
Rafał Kubiak <rafalkubiak@outlook.com>
2024-03-01 14:49:40 +0100

    Added authentication UI

---
+++ BookStore.UI.Srv/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+using BookStore.UI.Srv.Services.Base;
+using BookStore.UI.Srv.Services.Providers;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BookStore.UI.Srv.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    private readonly IClient httpClient;
+    private readonly ILocalStorageService localStorage;
+    private readonly AuthenticationStateProvider authenticationStateProvider;
+
+    public AuthenticationService(IClient httpClient, ILocalStorageService localStorage, AuthenticationStateProvider authenticationStateProvider)
+    {
+        this.httpClient = httpClient;
+        this.localStorage = localStorage;
+        this.authenticationStateProvider = authenticationStateProvider;
+    }
+    public async Task<bool> AuthenticateAsync(LoginUserDto loginModel)
+    {
+        var response = await httpClient.LoginAsync(loginModel);
+
+        //store token
+        await localStorage.SetItemAsync("accessToken", response.Token);
+
+        //change auth state 
+        await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedIn();
+
+
+        return true;
+    }
+}
---
+++ BookStore.UI.Srv/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿using BookStore.UI.Srv.Services.Base;
+
+namespace BookStore.UI.Srv.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    Task<bool> AuthenticateAsync(LoginUserDto loginModel);
+}
